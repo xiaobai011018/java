e6d4b669321d973babaa86ddbc09fcde20195bab
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-08-19 23:32:24 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,23 +1,39 @@
 package com.bsc.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+
+    public Employee(int id, int importance, List<Integer> subordinates) {
+        this.id = id;
+        this.importance = importance;
+        this.subordinates = subordinates;
+    }
+}
 public class Solution {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.firstUniqChar("leetcode"));
+        List<Employee> list = new ArrayList<>();
     }
-    public int firstUniqChar(String s) {
-        for(int i = 0;i < s.length();i++){
-            int right = s.length()-1;
-            while(i<right){
-                if(s.charAt(i)==s.charAt(right)){
-                    break;
-                }
-                right--;
-            }
-            if(i==right){
-                return i;
-            }
+    public int getImportance(List<Employee> employees, int id) {
+        int count = 0;
+        HashMap<Integer, Employee> map = new HashMap<>();
+        for (Employee em : employees) {
+            map.put(em.id, em);
         }
-        return -1;
+        return getCount(map,id);
+    }
+    public int getCount(Map<Integer,Employee> map,int id){
+        Employee employee = map.get(id);
+        int count = employee.importance;
+        for (Integer subordinate : employee.subordinates) {
+            count += getCount(map,subordinate);
+        }
+        return count;
     }
 }
