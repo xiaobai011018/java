cf6d64d1a9164cb233d2c10e2a4ca040e2e5ac26
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-08-27 23:43:59 +0800

    Add files via upload

---
+++ SomeServiceImp.java
@@ -0,0 +1,10 @@
+package com.bsc.service.impl;
+
+import com.bsc.service.SomeService;
+
+public class SomeServiceImp implements SomeService {
+    @Override
+    public void doSome() {
+        System.out.println("实现了SomeService接口");
+    }
+}
