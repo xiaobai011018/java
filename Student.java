ffbd65ebbde1a045cec64d40aea3bc940e0a1d37
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-08-28 23:21:36 +0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,50 +1,22 @@
-package com.bsc.domain;
+package com.bsc.ba01;
 
 public class Student {
-    private Integer id;
-    private String name;
-    private String email;
-    private Integer age;
+    String name;
+    int age;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
+    public void setAge(int age) {
         this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", age=" + age +
-                '}';
     }
 }
