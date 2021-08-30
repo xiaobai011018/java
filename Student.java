ffececb5031dfc4883e8bff781a66ee0716f7659
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-08-30 23:52:07 +0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,36 +1,25 @@
-package com.bsc.ba04;
+package com.bsc.ba01;
 
+import org.springframework.stereotype.Component;
+
+@Component(value = "myStudent")
 public class Student {
     String name;
-    int age;
-    School school;
-
-    public Student() {
-    }
-
-    public Student (String myname, int myage, School myschool){
-         name = myname;
-         age = myage;
-         school = myschool;
-    }
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", school=" + school +
-                '}';
-    }
+    Integer age;
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
-    public void setSchool(School school) {
-        this.school = school;
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
