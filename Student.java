0dc0d5b1a192a34c78e3c3467007651c42651bc9
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-03 23:25:41 +0800

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,25 +1,60 @@
-package com.bsc.ba01;
+package com.bsc.domain;
 
-import org.springframework.stereotype.Component;
-
-@Component(value = "myStudent")
 public class Student {
-    String name;
-    Integer age;
+    private Integer age;
+    private String name;
+    private String email;
+    private Integer id;
 
-    public void setName(String name) {
+    public Student() {
+    }
+
+    public Student(Integer age, String name, String email, Integer id) {
+        this.age = age;
         this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
     }
 
     public void setAge(Integer age) {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", id=" + id +
                 '}';
     }
 }
