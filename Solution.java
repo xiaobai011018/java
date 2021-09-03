a7f6d4ff8a0c8da8fa68fce572719365c35e3ba9
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-03 23:25:04 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,50 +1,68 @@
 package com.bsc.leetcode;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
-    public static int compareVersion(String version1, String version2) {
-        String[] vs1 = version1.split("\\.");
-        String[] vs2 = version2.split("\\.");
-        int length1 = vs1.length;
-        int length2 = vs2.length;
-        if(length1!=length2){
-            if(length1<length2){
-                for(int i = 0;i<length1;i++){
-                    if(comPare(vs1[i],vs2[i])!=0){
-                        return comPare(vs1[i],vs2[i]);
-                    }
-                }
-                for(int i = length1;i<length2;i++){
-                    if(Integer.parseInt(vs2[i])!=0){
-                        return -Integer.parseInt(vs2[i]);
-                    }
-                }
-                return 0;
-            }
-            if(length1>length2){
-                for(int i = 0;i<length2;i++){
-                    if(comPare(vs1[i],vs2[i])!=0){
-                        return comPare(vs1[i],vs2[i]);
-                    }
-                }
-                for(int i = length2;i<length1;i++){
-                    if(Integer.parseInt(vs1[i])!=0){
-                        return Integer.parseInt(vs1[i]);
-                    }
-                }
-                return 0;
-            }
-        }
-        for(int i = 0;i<length1;i++){
-            if(comPare(vs1[i],vs2[i])!=0){
-                return comPare(vs1[i],vs2[i]);
-            }
-        }
-        return 0;
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,2,4,6,8};
+        Solution solution = new Solution();
+        solution.smallestK(arr,4);
+//        solution.shellSort(arr);
+//        System.out.println(Arrays.toString(arr));
     }
-    public static int comPare(String s1,String s2){
-        int num1 = Integer.parseInt(s1);
-        int num2 = Integer.parseInt(s2);
-        return num1-num2;
+    public int[] smallestK(int[] arr, int k) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k,(o1, o2) -> o2-o1);
+        for (int i = 0; i < arr.length; i++) {
+            if (priorityQueue.size()<k){
+                priorityQueue.offer(arr[i]);
+            }else {
+                if (priorityQueue.peek()>arr[i]){
+                    priorityQueue.poll();
+                    priorityQueue.offer(arr[i]);
+                }
+            }
+        }
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = priorityQueue.poll();
+        }
+        return result;
+    }
+    public void shellSort(int[] arr){
+        int gap = arr.length;
+        while (gap>1){
+            gap /= 2;
+            shell(arr,gap);
+        }
+        shell(arr,1);
+    }
+    public void shell(int[] arr,int gap){
+        for (int i = gap; i < arr.length; i++) {
+            int j = i-gap;
+            int tmp = arr[i];
+            for ( ; j >= 0 ; j -= gap) {
+                if (arr[j+gap]<arr[j]){
+                    arr[j+gap] = arr[j];
+                }else {
+                    break;
+                }
+            }
+            arr[j+gap] = tmp;
+        }
+    }
+    public void insertSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i-1; j >=0 ; j--) {
+               if (arr[i]<arr[j]){
+                   int tmp = arr[i];
+                   arr[i] = arr[j];
+                   arr[j] = tmp;
+                   i = j;
+               }else {
+                   break;
+               }
+            }
+        }
     }
 }
