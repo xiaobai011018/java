50820f72ef4a3e96f29a5848d1ca06f15fd142a7
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-08 17:13:04 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -4,65 +4,27 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Solution {
-    public static void main(String[] args) {
-        int[] arr = {1,3,5,7,2,4,6,8};
-        Solution solution = new Solution();
-        solution.smallestK(arr,4);
-//        solution.shellSort(arr);
-//        System.out.println(Arrays.toString(arr));
-    }
-    public int[] smallestK(int[] arr, int k) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k,(o1, o2) -> o2-o1);
-        for (int i = 0; i < arr.length; i++) {
-            if (priorityQueue.size()<k){
-                priorityQueue.offer(arr[i]);
-            }else {
-                if (priorityQueue.peek()>arr[i]){
-                    priorityQueue.poll();
-                    priorityQueue.offer(arr[i]);
-                }
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital){
+        int n = profits.length;
+        int curr = 0;
+        int[][] arr = new int[n][2];
+        for(int i = 0;i<n;i++){
+            arr[i][0] = capital[i];
+            arr[i][1] = profits[i];
+        }
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y)-> y-x);
+        for(int i = 0;i<k;i++){
+            while(curr<n&&arr[curr][0]<=w){
+                pq.offer(arr[curr][1]);
+                curr++;
+            }
+            if(!pq.isEmpty()){
+                w += pq.poll();
+            }else{
+                break;
             }
         }
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = priorityQueue.poll();
-        }
-        return result;
-    }
-    public void shellSort(int[] arr){
-        int gap = arr.length;
-        while (gap>1){
-            gap /= 2;
-            shell(arr,gap);
-        }
-        shell(arr,1);
-    }
-    public void shell(int[] arr,int gap){
-        for (int i = gap; i < arr.length; i++) {
-            int j = i-gap;
-            int tmp = arr[i];
-            for ( ; j >= 0 ; j -= gap) {
-                if (arr[j+gap]<arr[j]){
-                    arr[j+gap] = arr[j];
-                }else {
-                    break;
-                }
-            }
-            arr[j+gap] = tmp;
-        }
-    }
-    public void insertSort(int[] arr){
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i-1; j >=0 ; j--) {
-               if (arr[i]<arr[j]){
-                   int tmp = arr[i];
-                   arr[i] = arr[j];
-                   arr[j] = tmp;
-                   i = j;
-               }else {
-                   break;
-               }
-            }
-        }
+        return w;
     }
 }
