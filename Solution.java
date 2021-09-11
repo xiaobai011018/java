604a024bcbb617c0f75277e3f11061f3bb464101
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-11 23:42:22 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -2,34 +2,58 @@ package com.bsc.leetcode;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] chalk = {5,1,5};
+        int[] prices = {6,1,3,2,4,7};
         Solution solution = new Solution();
-        System.out.println(solution.chalkReplacer(chalk, 22));
+        solution.binaryToDecimal(3);
+        String s = "123";
+//        solution.maxProfit(prices);
     }
-    public int chalkReplacer(int[] chalk, int k) {
-        int i = 0;
-        while(k>chalk[0]){
-            for(i=0;i<chalk.length;i++){
-                if(k<chalk[i]){
-                    return i;
+    public int findIntegers(int n) {
+        int result = 0;
+        for(int i = 0;i<=n;i++){
+            char[] ch = Integer.toBinaryString(i).toCharArray();
+            int j = 0;
+            for(;j<ch.length-1;j++){
+                if(ch[j]=='1'&&ch[j+1]=='1'){
+                    break;
                 }
-                k -= chalk[i];
             }
-        }
-        return 0;
-    }
-    public int maxSubArray(int[] nums) {
-        int result = Integer.MIN_VALUE;
-        int count = 0;
-        for(int i = 0;i<nums.length;i++){
-            count += nums[i];
-            if (count>result){
-                result = count;
-            }
-            if (result<0){
-                count = 0;
+            if(j==ch.length-1){
+                result++;
             }
         }
         return result;
+    }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int count = 0;
+        int buy = 0;
+        int sell = 0;
+        boolean buy_flg = false;
+        boolean sell_flg = false;
+        for(int i = 0;i<n-1;i++){
+            if(prices[i]<prices[i+1]){
+                if(buy==0){
+                    buy = prices[i];
+                    buy_flg = true;
+                }
+            }else{
+                if(buy!=0){
+                    sell = prices[i];
+                    count += sell-buy;
+                    buy = 0;
+                    sell_flg = true;
+                }
+            }
+        }
+        if(buy_flg&&!sell_flg){
+            count = prices[n-1] - prices[0];
+        }
+        return count;
+    }
+    public void binaryToDecimal(int n){
+        for(int i = 31;i >= 0; i--) {
+            System.out.print(n >>> i & 1);
+        }
     }
 }
