84f91a7c9ddea4884896475e1bf63fdc948090e5
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-13 23:33:33 +0800

    Add files via upload

--- MyController.java
+++ MyController.java
@@ -2,15 +2,25 @@ package com.bsc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
+@RequestMapping(value = "/test")
 public class MyController {
-    @RequestMapping(value = "/some.do")
+    @RequestMapping(value = "/some.do",method = RequestMethod.GET)
     public ModelAndView doSome(){
         ModelAndView mv = new ModelAndView();
         mv.addObject("msg","这是第一个springmvc程序");
         mv.addObject("func","这是第一个函数");//request.getContext.setAttrubut
-        mv.setViewName("/show.jsp");//相当于请求转发
+        mv.setViewName("show");//相当于请求转发
+        return mv;
+    }
+    @RequestMapping(value = "/other.do",method = RequestMethod.GET)
+    public ModelAndView doOther(){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg","这是第一个springmvc程序");
+        mv.addObject("func","这是第一个函数");//request.getContext.setAttrubut
+        mv.setViewName("show");//相当于请求转发
         return mv;
     }
 }
