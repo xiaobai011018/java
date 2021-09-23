da91e6a1d4b16e48a0fb203e80bc0120f57d7f9a
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-23 23:13:47 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,35 +1,19 @@
 package com.bsc.leetcode;
 
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
-
 public class Solution {
-    public ListNode[] splitListToParts(ListNode head, int k) {
-        int n = 0;
-        ListNode cur = head;
-        while(cur!=null){
-            n++;
-            cur = cur.next;
-        }
-        ListNode[] parts = new ListNode[k];
-        cur = head;
-        int quo = n/k;
-        int red = n%k;
-        for(int i = 0;i<k&&cur!=null;i++){
-            parts[i] = cur;
-            int foot = quo + (i<red?1:0);
-            for(int j = 1;i<foot;j++){
-                cur = cur.next;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPowerOfThree(2147483647));
+    }
+    public boolean isPowerOfThree(int n) {
+        long num = 1;
+        while(num<=n){
+            if(n==num){
+                return true;
             }
-            ListNode next = cur.next;
-            cur.next = null;
-            cur = next;
+            num *= 3;
+
         }
-        return parts;
+        return false;
     }
 }
