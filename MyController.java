fe6c11af2d6fb5262a60b05a67a266df5a0245e8
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-25 23:24:09 +0800

    Add files via upload

--- MyController.java
+++ MyController.java
@@ -1,23 +1,17 @@
 package com.bsc.controller;
 
-import com.bsc.vo.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
-    @RequestMapping(value = "/returnstring.do")
-    public String doSome(HttpServletRequest request, String name, int age){
+    @RequestMapping(value = "/some.do")
+    public ModelAndView doSome(String name,Integer age)  {
         ModelAndView mv = new ModelAndView();
-        request.setAttribute("name",name);
-        request.setAttribute("age",age);
-        return "show";
+        mv.addObject("myname",name);
+        mv.addObject("myage",age);
+        mv.setViewName("show");
+        return mv;
     }
-
 }
