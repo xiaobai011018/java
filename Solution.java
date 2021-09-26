b48b86c371cd84404bc9e79b7e85a86d5ecddb17
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-09-26 23:33:57 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,22 +1,12 @@
 package com.bsc.leetcode;
 
 public class Solution {
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i = 1;i<=n;i++){
-            char c1 = word1.charAt(i-1);
-            for(int j = 0;j<=m;j++){
-                char c2 = word2.charAt(j-1);
-                if(c1==c2){
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+    public int getSum(int a, int b) {
+        while(b!=0){
+            int carry = (a&b)<<1;
+            a ^= b;
+            b = carry;
         }
-        int maxLength = dp[n][m];
-        return n-maxLength+m-maxLength;
+        return a;
     }
 }
