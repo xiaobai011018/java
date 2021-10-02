b6ad3c8655ce4501c0df33545e59a1c606969bc9
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-10-02 22:10:01 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,47 +1,51 @@
 package com.bsc.leetcode;
 
+import java.util.*;
+
 public class Solution {
-    static final int MOD = 1000000007;
-
-    public int numDecodings(String s) {
-        int n = s.length();
-        // a = f[i-2], b = f[i-1], c = f[i]
-        long a = 0, b = 1, c = 0;
-        for (int i = 1; i <= n; ++i) {
-            c = b * check1digit(s.charAt(i - 1)) % MOD;
-            if (i > 1) {
-                c = (c + a * check2digits(s.charAt(i - 2), s.charAt(i - 1))) % MOD;
-            }
-            a = b;
-            b = c;
+    public static void main(String[] args) {
+//        Queue queue = new PriorityQueue();
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        while (num!=0){
+            System.out.print(num%2);
+            num /= 2;
         }
-        return (int) c;
     }
-
-    public int check1digit(char ch) {
-        if (ch == '0') {
-            return 0;
+    public String destCity(List<List<String>> paths) {
+        HashMap<String,String> map = new HashMap<>();
+        for(List<String> s:paths){
+            map.put(s.get(0),s.get(1));
         }
-        return ch == '*' ? 9 : 1;
+        String ans = paths.get(0).get(0);
+        while(map.containsKey(ans)){
+            ans = map.get(ans);
+        }
+        return ans;
     }
-
-    public int check2digits(char c0, char c1) {
-        if (c0 == '*' && c1 == '*') {
-            return 15;
+    public String toHex(int _num) {
+        long v = 0;
+        StringBuffer sb = new StringBuffer();
+        long num = (long)_num;
+        if(num==0) return "0";
+        if(num<0) {
+            num = (long)(Math.pow(2,32)+num);
         }
-        if (c0 == '*') {
-            return c1 <= '6' ? 2 : 1;
-        }
-        if (c1 == '*') {
-            if (c0 == '1') {
-                return 9;
+        while(num!=0){
+            v = num%16;
+            char c = (char)(v+'0');
+            if(v>=10){
+                c = (char)(v-10+'a');
             }
-            if (c0 == '2') {
-                return 6;
-            }
-            return 0;
+            sb.append(c);
+            num /= 16;
         }
-        return (c0 != '0' && (c0 - '0') * 10 + (c1 - '0') <= 26) ? 1 : 0;
+        return sb.reverse().toString();
     }
-
+//    public String toHex(int num) {
+//        while (num!=0){
+//            System.out.print(num%2);
+//            num /= 2;
+//        }
+//    }
 }
