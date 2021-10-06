5838842f1762d8f9cc94018b3f6d8d5f2eb3f5e4
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-10-06 15:50:47 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,21 +1,13 @@
 package com.bsc.leetcode;
 
+import java.util.*;
+
 public class Solution {
-    public String licenseKeyFormatting(String s, int k) {
-        int cnt = 0;
-        StringBuffer sb = new StringBuffer();
-        for(int i = s.length()-1;i>=0;i--){
-            if(s.charAt(i)!='-'){
-                cnt++;
-                sb.append(Character.toUpperCase(s.charAt(i)));
-                if(cnt%k==0){
-                    sb.append("-");
-                }
-            }
-        }
-        if(sb.length()>0&&sb.charAt(sb.length()-1)=='-'){
-            sb.deleteCharAt(sb.length()-1);
-        }
-        return sb.reverse().toString();
+    public int thirdMax(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num:nums) set.add(num);
+        ArrayList<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        return list.size()>=3?list.get(list.size()-3):list.get(list.size()-1);
     }
 }
