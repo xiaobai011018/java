b01ecfdd2ca8a57cd8bc37b735ba04998e854740
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-10-07 21:16:53 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,13 +1,13 @@
 package com.bsc.leetcode;
 
-import java.util.*;
-
 public class Solution {
-    public int thirdMax(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num:nums) set.add(num);
-        ArrayList<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
-        return list.size()>=3?list.get(list.size()-3):list.get(list.size()-1);
+    public int countSegments(String s) {
+        int ans = 0;
+        for(int i = 0;i<s.length();i++){
+            if((i==0||s.charAt(i-1)==' ')&&s.charAt(i)!=' '){
+                ans++;
+            }
+        }
+        return ans;
     }
 }
