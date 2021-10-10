bed86e870119a8af830a9616e13175e46a5de495
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-10-10 22:48:00 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,23 +1,18 @@
 package com.bsc.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Solution {
-    static final int l = 10;
-    public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans = new ArrayList<>();
-        Map<String,Integer> cnt = new HashMap<>();
-        int n = s.length();
-        for(int i = 0;i<=n-l;i++){
-            String sub = s.substring(i,i+l);
-            cnt.put(sub,cnt.getOrDefault(sub,0)+1);
-            if (cnt.get(sub)==2){
-                ans.add(sub);
-            }
+    public static void main(String[] args) {
+        System.out.println(arrangeCoins(5));
+    }
+    public static int arrangeCoins(int n) {
+        int i = 0;
+        while (n>=0){
+            i++;
+            n -= i;
         }
-        return ans;
+        if (n<0){
+            return i--;
+        }
+        return i;
     }
 }
