e943b636bc093c639eb0fec23cb142884e090b26
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-10-13 23:30:42 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,18 +1,30 @@
 package com.bsc.leetcode;
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println(arrangeCoins(5));
+    int INF = Integer.MAX_VALUE;
+    public int divide(int _a, int _b) {
+        long a = _a, b = _b;
+        boolean flag = false;
+        if ((a < 0 && b > 0) || (a > 0 && b < 0)) flag = true;
+        if (a < 0) a = -a;
+        if (b < 0) b = -b;
+        long l = 0, r = a;
+        while (l < r) {
+            long mid = l + r + 1 >> 1;
+            if (mul(mid, b) <= a) l = mid;
+            else r = mid - 1;
+        }
+        r = flag ? -r : r;
+        if (r > INF || r < -INF - 1) return INF;
+        return (int)r;
     }
-    public static int arrangeCoins(int n) {
-        int i = 0;
-        while (n>=0){
-            i++;
-            n -= i;
+    long mul(long a, long k) {
+        long ans = 0;
+        while (k > 0) {
+            if ((k & 1) == 1) ans += a;
+            k >>= 1;
+            a += a;
         }
-        if (n<0){
-            return i--;
-        }
-        return i;
+        return ans;
     }
 }
