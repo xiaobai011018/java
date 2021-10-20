c81494604ec178bfd6d46030061a308691c4f364
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-10-20 23:31:27 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,47 +1,14 @@
 package com.bsc.leetcode;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
-public class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer> list = new ArrayList<>();
-        def(root,list);
-        Collections.sort(list);
-        return list.get(k-1);
-    }
-    void def(TreeNode node,ArrayList list){
-        if(node==null){
-            return;
+class Solution {
+    public int minMoves(int[] nums) {
+        int minNum = Arrays.stream(nums).min().getAsInt();
+        int res = 0;
+        for (int num : nums) {
+            res += num - minNum;
         }
-        list.add(node.val);
-        def(node.left,list);
-        def(node.right,list);
-    }
-    public int findComplement(int num) {
-        int s = -1;
-        for (int i = 31; i >= 0; i--) {
-            if (((num >> i) & 1) != 0) {
-                s = i;
-                break;
-            }
-        }
-        int ans = 0;
-        for (int i = 0; i < s; i++) {
-            if (((num >> i) & 1) == 0) ans |= (1 << i);
-        }
-        return ans;
+        return res;
     }
 }
