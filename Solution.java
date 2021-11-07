ab976c0864f621ed955dd5d3f5b8f57da58f7646
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-11-07 23:46:54 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,12 +1,26 @@
 package com.bsc.leetcode;
-   class ListNode {
-      int val;
-      ListNode next;
-      ListNode(int x) { val = x; }
-  }
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public void deleteNode(ListNode node) {
-        node.val = node.next.val;
-        node.next = node.next.next;
+    public int maxCount(int m, int n, int[][] ops) {
+        int[][] arr = new int[m][n];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int[] num:ops){
+            for(int i = 0;i<num[0];i++){
+                for(int j = 0;j<num[1];j++){
+                    arr[i][j] = arr[i][j]+1;
+                }
+            }
+        }
+        int ans = -99999;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                map.put(arr[i][j],map.getOrDefault(arr[i][j],0)+1);
+                ans = Math.max(arr[i][j],ans);
+            }
+        }
+        return map.get(ans);
     }
 }
