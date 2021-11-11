3a1ac525d527f7436216840c23a77960ec747d9c
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-11-11 23:46:13 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,16 +1,25 @@
 package com.bsc.leetcode;
 class Solution {
-    public int findPoisonedDuration(int[] timeSeries, int duration) {
-        int ans = 0;
-        int expired = 0;
-        for (int i = 0; i < timeSeries.length; ++i) {
-            if (timeSeries[i] >= expired) {
-                ans += duration;
-            } else {
-                ans += timeSeries[i] + duration - expired;
+    public int kInversePairs(int n, int k) {
+        final int MOD = 1000000007;
+        int[][] f = new int[2][k + 1];
+        f[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= k; ++j) {
+                int cur = i & 1, prev = cur ^ 1;
+                f[cur][j] = (j - 1 >= 0 ? f[cur][j - 1] : 0) - (j - i >= 0 ? f[prev][j - i] : 0) + f[prev][j];
+                if (f[cur][j] >= MOD) {
+                    f[cur][j] -= MOD;
+                } else if (f[cur][j] < 0) {
+                    f[cur][j] += MOD;
+                }
             }
-            expired = timeSeries[i] + duration;
         }
-        return ans;
+        return f[n & 1][k];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.kInversePairs(3,4);
     }
 }
