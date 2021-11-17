e7f49760e04a28e68190a5d3758122428cdc1998
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-11-17 23:39:26 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,55 +1,38 @@
-package com.bsc.leetcode;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-class Solution {
-    public boolean isRectangleCover(int[][] rectangles) {
-        int n = rectangles.length;
-        int[][] rs = new int[n * 2][4];
-        for (int i = 0, idx = 0; i < n; i++) {
-            int[] re = rectangles[i];
-            rs[idx++] = new int[]{re[0], re[1], re[3], 1};
-            rs[idx++] = new int[]{re[2], re[1], re[3], -1};
-        }
-        Arrays.sort(rs, (a, b)->{
-            if (a[0] != b[0]) return a[0] - b[0];
-            return a[1] - b[1];
-        });
-        n *= 2;
-        // 分别存储相同的横坐标下「左边的线段」和「右边的线段」 (y1, y2)
-        List<int[]> l1 = new ArrayList<>(), l2 = new ArrayList<>();
-        for (int l = 0; l < n; ) {
-            int r = l;
-            l1.clear(); l2.clear();
-            // 找到横坐标相同部分
-            while (r < n && rs[r][0] == rs[l][0]) r++;
-            for (int i = l; i < r; i++) {
-                int[] cur = new int[]{rs[i][1], rs[i][2]};
-                List<int[]> list = rs[i][3] == 1 ? l1 : l2;
-                if (list.isEmpty()) {
-                    list.add(cur);
-                } else {
-                    int[] prev = list.get(list.size() - 1);
-                    if (cur[0] < prev[1]) return false; // 存在重叠
-                    else if (cur[0] == prev[1]) prev[1] = cur[1]; // 首尾相连
-                    else list.add(cur);
+public class Solution {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            String str1 = words[i];
+            for(int j = i+1;j<n;j++){
+                String str2 = words[j];
+                if (!isSamWord(str1,str2)){
+                    ans = Math.max(str1.length()*str2.length(),ans);
                 }
             }
-            if (l > 0 && r < n) {
-                // 若不是完美矩形的边缘竖边，检查是否成对出现
-                if (l1.size() != l2.size()) return false;
-                for (int i = 0; i < l1.size(); i++) {
-                    if (l1.get(i)[0] == l2.get(i)[0] && l1.get(i)[1] == l2.get(i)[1]) continue;
-                    return false;
-                }
-            } else {
-                // 若是完美矩形的边缘竖边，检查是否形成完整一段
-                if (l1.size() + l2.size() != 1) return false;
-            }
-            l = r;
         }
-        return true;
+        return ans;
+    }
+
+    private boolean isSamWord1(String str1, String str2) {
+        int[] count = new int[26];
+        for(char ch:str1.toCharArray()){
+            count[ch-'a'] = 1;
+        }
+        for(char ch : str2.toCharArray()){
+            if (count[ch-'a']==1) return true;
+        }
+        return false;
+    }
+    private boolean isSamWord(String str1, String str2) {
+        int bitMask1 = 0;
+        int bitMask2 = 0;
+        for(char ch:str1.toCharArray()){
+            bitMask1 |= 1<<(ch-'a');
+        }
+        for(char ch : str2.toCharArray()){
+            bitMask2 |= 1<<(ch-'a');
+        }
+        return (bitMask1&bitMask2)!=0;
     }
 }
