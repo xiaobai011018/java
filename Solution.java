b8780e02a71f994cdf054811d1aae1e67d2908c4
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-11-18 23:53:34 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,38 +1,27 @@
+ class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 public class Solution {
-    public int maxProduct(String[] words) {
-        int n = words.length;
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            String str1 = words[i];
-            for(int j = i+1;j<n;j++){
-                String str2 = words[j];
-                if (!isSamWord(str1,str2)){
-                    ans = Math.max(str1.length()*str2.length(),ans);
-                }
-            }
-        }
+    int ans = 0;
+    public int findTilt(TreeNode root) {
+        if(root==null) return 0;
+        slop(root);
         return ans;
     }
-
-    private boolean isSamWord1(String str1, String str2) {
-        int[] count = new int[26];
-        for(char ch:str1.toCharArray()){
-            count[ch-'a'] = 1;
-        }
-        for(char ch : str2.toCharArray()){
-            if (count[ch-'a']==1) return true;
-        }
-        return false;
-    }
-    private boolean isSamWord(String str1, String str2) {
-        int bitMask1 = 0;
-        int bitMask2 = 0;
-        for(char ch:str1.toCharArray()){
-            bitMask1 |= 1<<(ch-'a');
-        }
-        for(char ch : str2.toCharArray()){
-            bitMask2 |= 1<<(ch-'a');
-        }
-        return (bitMask1&bitMask2)!=0;
+    int slop(TreeNode node){
+        if(node==null) return 0;
+        int left = slop(node.left);
+        int right = slop(node.right);
+        ans += Math.abs(left-right);
+        return node.val+right+left;
     }
 }
