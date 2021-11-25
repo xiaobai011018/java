999dd4ef7faaf56a0743847628265d01f8a1f5bb
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-11-25 23:59:59 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,32 +1,13 @@
-import java.util.HashMap;
-
 public class Solution {
     public static void main(String[] args) {
-        buddyStrings("ab","ba");
+        System.out.println(Math.ceil(4.2));
     }
-    public static boolean buddyStrings(String s, String goal) {
-        int n = s.length();
-        int m = goal.length();
-        int sum = 0;
-        if (m!=n) return false;
-        int[] cnt1 = new int[26];
-        int[] cnt2 = new int[26];
-        for (int i = 0;i<n;i++){
-            int a = s.charAt(i)-'a';
-            int b = goal.charAt(i)-'a';
-            cnt1[a]++;
-            cnt2[b]++;
-            if (a!=b){
-                sum++;
-            }
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pig = 0;
+        int state = minutesToTest/minutesToDie+1;
+        while(Math.pow(state,pig)<buckets){
+            pig++;
         }
-        boolean ok = false;
-        for (int i = 0;i<26;i++){
-            if (cnt1[i]!=cnt2[i]) return false;
-            if (cnt1[i]>1){
-                ok = true;
-            }
-        }
-        return sum==2||(sum==0&&ok);
+        return pig;
     }
 }
