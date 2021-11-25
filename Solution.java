e1208cc2343f928c44c45a531bb9a665db59953c
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-11-25 23:59:29 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,28 +1,32 @@
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashMap;
 
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-}
 public class Solution {
-    public int maxDepth(Node root) {
-        int ans = 0;
-        Iterator<Node> iterator = root.children.iterator();
-        while (iterator.hasNext()){
-            Node node = iterator.next();
-            
+    public static void main(String[] args) {
+        buddyStrings("ab","ba");
+    }
+    public static boolean buddyStrings(String s, String goal) {
+        int n = s.length();
+        int m = goal.length();
+        int sum = 0;
+        if (m!=n) return false;
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0;i<n;i++){
+            int a = s.charAt(i)-'a';
+            int b = goal.charAt(i)-'a';
+            cnt1[a]++;
+            cnt2[b]++;
+            if (a!=b){
+                sum++;
+            }
         }
+        boolean ok = false;
+        for (int i = 0;i<26;i++){
+            if (cnt1[i]!=cnt2[i]) return false;
+            if (cnt1[i]>1){
+                ok = true;
+            }
+        }
+        return sum==2||(sum==0&&ok);
     }
 }
