25f8f0f40ea392b5a7a54d1328c750d03cd46a66
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2021-12-01 23:42:35 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,13 +1,15 @@
- public class Solution {
-    public int findNthDigit(int n) {
-        int len = 1;
-        while (len * 9 * Math.pow(10, len - 1) < n) {
-            n -= len * 9 * Math.pow(10, len - 1);
-            len++;
+import java.util.HashMap;
+
+public class Solution {
+    public int maxPower(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for (char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
-        long s = (long) Math.pow(10, len - 1);
-        long x = n / len - 1 + s;
-        n -= (x - s + 1) * len;
-        return n == 0 ? (int) (x % 10) : (int) ((x + 1) / Math.pow(10, len - n) % 10);
+        int ans = 0;
+        for (Character ch:map.keySet()){
+            ans = Math.max(ans,map.get(ch));
+        }
+        return ans;
     }
 }
