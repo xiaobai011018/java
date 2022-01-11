ddb9038420d75dde56c387b2a672b7a2d33b2a09
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2022-01-11 22:18:28 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,12 +1,28 @@
+package leetcode;
+
 public class Solution {
-    public String toLowerCase(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c:s.toCharArray()){
-            if ( c>=65 && c<=90){
-                c += 32;
+    public int jump(int[] nums) {
+        int ans = 0;
+        int cur = 0;
+        int next = 0;
+        for (int i = 0; i < nums.length; i++) {
+            next = Math.max(i+nums[i],next);
+            if (i==cur){
+                if (cur<nums.length-1){
+                    ans++;
+                    cur = next;
+                }else break;
             }
-            sb.append(c);
         }
-        return sb.toString();
+        return ans;
+    }
+    public boolean canJump(int[] nums) {
+        if (nums.length==1) return true;
+        int cover = 0;
+        for (int i = 0; i <= cover; i++) {
+            cover = Math.max(i+nums[i],cover);
+            if (cover>=nums.length-1) return true;
+        }
+        return false;
     }
 }
