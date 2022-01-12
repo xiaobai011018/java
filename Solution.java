60b7ef810d389d9e3c42fc04a8cd723a25664d22
柏诗成 <76875817+xiaobai011018@users.noreply.github.com>
2022-01-12 22:42:50 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,28 +1,64 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Solution {
-    public int jump(int[] nums) {
-        int ans = 0;
-        int cur = 0;
-        int next = 0;
-        for (int i = 0; i < nums.length; i++) {
-            next = Math.max(i+nums[i],next);
-            if (i==cur){
-                if (cur<nums.length-1){
-                    ans++;
-                    cur = next;
-                }else break;
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int curSum = 0;
+        int totalSum = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if (curSum<0){
+                start = i+1;
+                curSum = 0;
             }
         }
-        return ans;
+        if (totalSum<0) return -1;
+        return start;
     }
-    public boolean canJump(int[] nums) {
-        if (nums.length==1) return true;
-        int cover = 0;
-        for (int i = 0; i <= cover; i++) {
-            cover = Math.max(i+nums[i],cover);
-            if (cover>=nums.length-1) return true;
+    public int canCompleteCircuit1(int[] gas, int[] cost) {
+        int ret = -1;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < gas.length; i++) {
+            if (gas[i]>cost[i]){
+                map.put(i,gas[i]);
+            }
         }
-        return false;
+        if (map.size()==0) return -1;
+        int ans = 0;
+        int k = gas.length;
+        for (Integer index : map.keySet()) {
+            while (k!=0){
+                ans += gas[index]-cost[index]+gas[index+1];
+                index++;
+                index %= gas.length;
+                k--;
+            }
+            if (ans>=0){
+                ret = index;
+            }
+        }
+        return ret;
+    }
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (k!=0&&nums[i]<0){
+                nums[i] = -nums[i];
+                k--;
+
+            }
+        }
+        Arrays.sort(nums);
+        if (k%2==1){
+            nums[0] = -nums[0];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            ans += nums[i];
+        }
+        return ans;
     }
 }
